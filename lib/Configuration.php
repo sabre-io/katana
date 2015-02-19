6e60f957369fb42fbe67bf7fe1003854e45a2038
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-02-19 15:41:47 +0100

    Add the `Sabre\Katana\Configuration` class.
    Also to open, read and write JSON configurations.

---
+++ lib/Configuration.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Sabre\Katana;
+
+use Sabre\Katana\Exception;
+use StdClass;
+
+/**
+ * Open, read and write JSON configurations. No lock on the filesystem.
+ *
+ * @copyright Copyright (C) 2015 fruux GmbH (https://fruux.com/).
+ * @author Ivan Enderlin
+ * @license http://sabre.io/license/ Modified BSD License
+ */
+class Configuration
+{
+    /**
+     * Name of the configuration file.
+     *
+     * @var string
+     */
+    protected $_filename      = null;
+
+    /**
+     * Configurations as a StdClass.
+     *
+     * @var StdClass
+     */
+    protected $_configuration = null;
+
+    /**
+     * Read the configurations.
+     *
+     * @param  string  $filename    Filename to a valid JSON file.
+     * @return void
+     * @throw  Exception\Environment
+     */
+    public function __construct($filename)
+    {
+        if (false === file_exists($filename)) {
+            throw new Exception\Environment(
+                sprintf('The %s configuration file is not found.', $filename)
+            );
+        }
+
+        $this->_filename = $filename;
+        $decodedJson     = @json_decode(file_get_contents($filename));
+
+        if (!($decodedJson instanceof StdClass)) {
+            throw new Exception\Environment(
+                sprintf('The %s configuration contains invalid JSON data.', $filename)
+            );
+        }
+
+        $this->_configuration = $decodedJson;
+
+        return;
+    }
+
+    /**
+     * Check if an entry exists.
+     *
+     * @param  string  $name    Entry name.
+     * @return boolean
+     */
+    public function __isset($name)
+    {
+        return isset($this->_configuration->$name);
+    }
+
+    /**
+     * Read an entry.
+     *
+     * @param  string  $name    Entry name.
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        return $this->_configuration->$name;
+    }
+
+    /**
+     * Set an entry value. If it does not exist, it will create it.
+     *
+     * @param  string  $name     Entry name.
+     * @param  mixed   $value    Entry value.
+     * @return void
+     */
+    public function __set($name, $value)
+    {
+        $this->_configuration->$name = $value;
+
+        return;
+    }
+
+    /**
+     * Unset an entry.
+     *
+     * @param  string  $name     Entry name.
+     * @return void
+     */
+    public function __unset($name)
+    {
+        unset($this->_configuration->$name);
+
+        return;
+    }
+
+    /**
+     * Save the configurations into the original filename.
+     *
+     * @return boolean
+     */
+    public function save()
+    {
+        return false !== file_put_contents(
+            $this->getFilename(),
+            json_encode($this->_configuration)
+        );
+    }
+
+    /**
+     * Get the name of the read configuration file.
+     *
+     * @return string
+     */
+    public function getFilename()
+    {
+        return $this->_filename;
+    }
+}
---
+++ tests/Unit/Configuration.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Sabre\Katana\Test\Unit;
+
+use Sabre\Katana\Configuration as LUT;
+use atoum\mock\streams\fs\file;
+
+/**
+ * Test suite of the configuration component.
+ *
+ * @copyright Copyright (C) 2015 fruux GmbH (https://fruux.com/).
+ * @author Ivan Enderlin
+ * @license http://sabre.io/license/ Modified BSD License
+ */
+class Configuration extends Suite
+{
+    public function case_file_not_found()
+    {
+        $this
+            ->exception(function () {
+                new LUT('/foo/42');
+            })
+                ->isInstanceOf('Sabre\Katana\Exception\Environment');
+    }
+
+    public function case_invalid_json()
+    {
+        $this
+            ->given(
+                $file = (string) file::get('configuration.json'),
+                file_put_contents($file, 'x')
+            )
+            ->exception(function () use($file) {
+                new LUT($file);
+            })
+                ->isInstanceOf('Sabre\Katana\Exception\Environment');
+    }
+
+    public function case_get_filename()
+    {
+        $this
+            ->given(
+                $file          = $this->getFile(),
+                $configuration = new LUT($file)
+            )
+            ->when($result = $configuration->getFilename())
+            ->then
+                ->string($result)
+                    ->isEqualTo($file);
+    }
+
+    public function case_isset()
+    {
+        $this
+            ->given($configuration = new LUT($this->getFile()))
+            ->when($result = isset($configuration->a))
+            ->then
+                ->boolean($result)
+                    ->isTrue()
+
+            ->when($result = isset($configuration->z))
+            ->then
+                ->boolean($result)
+                    ->isFalse();
+    }
+
+    public function case_get()
+    {
+        $this
+            ->given($configuration = new LUT($this->getFile()))
+            ->when($result = $configuration->a)
+            ->then
+                ->integer($result)
+                    ->isEqualTo(42)
+
+            ->when($result = $configuration->b)
+            ->then
+                ->boolean($result)
+                    ->isTrue()
+
+            ->when($result = $configuration->c->foo)
+            ->then
+                ->string($result)
+                    ->isEqualTo('bar');
+    }
+
+    public function case_set()
+    {
+        $this
+            ->given($configuration = new LUT($this->getFile()))
+            ->when($result = $configuration->a)
+            ->then
+                ->integer($result)
+                    ->isEqualTo(42)
+
+            ->when(
+                $configuration->a = 153,
+                $result = $configuration->a
+            )
+            ->then
+                ->integer($result)
+                    ->isEqualTo(153)
+
+            ->when(
+                $configuration->new = '💩',
+                $result = $configuration->new
+            )
+            ->then
+                ->string($result)
+                    ->isEqualTo('💩');
+    }
+
+    public function case_unset()
+    {
+        $this
+            ->given($configuration = new LUT($this->getFile()))
+            ->when($result = isset($configuration->a))
+            ->then
+                ->boolean($result)
+                    ->isTrue()
+
+            ->when(function () use($configuration) {
+                unset($configuration->z);
+            })
+            ->and($result = isset($configuration->z))
+            ->then
+                ->boolean($result)
+                    ->isFalse();
+    }
+
+    public function case_save()
+    {
+        $this
+            ->given(
+                $file           = $this->getFile(),
+                $initialContent = file_get_contents($file),
+                $configuration  = new LUT($file)
+            )
+            ->when($result = $configuration->save())
+            ->then
+                ->boolean($result)
+                    ->isTrue()
+                ->string(file_get_contents($file))
+                    ->isEqualTo($initialContent)
+
+            ->when(
+                $configuration->a = 153,
+                $result           = $configuration->save()
+            )
+            ->then
+                ->boolean($result)
+                    ->isTrue()
+                ->string(file_get_contents($file))
+                    ->isNotEqualTo($initialContent)
+
+            ->given($configuration = new LUT($file))
+            ->when($result = $configuration->a)
+            ->then
+                ->integer($result)
+                    ->isEqualTo(153);
+    }
+
+    protected function getFile()
+    {
+        $file = (string) file::get('configuration.json');
+        file_put_contents(
+            $file,
+            json_encode([
+                'a' => 42,
+                'b' => true,
+                'c' => [
+                    'foo' => 'bar'
+                ]
+            ])
+        );
+
+        return $file;
+    }
+}
