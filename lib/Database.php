c0f1f2bf4a7b000f92f797106c15dc4404aa03e8
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-02-19 15:43:59 +0100

    Add the database layer.
    So far, we use a simple wrapper of PDO.

---
+++ lib/Database.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sabre\Katana;
+
+use PDO;
+
+/**
+ * This class represents the connection to the database.
+ *
+ * @copyright Copyright (C) 2015 fruux GmbH (https://fruux.com/).
+ * @author Ivan Enderlin
+ * @license http://sabre.io/license/ Modified BSD License
+ */
+class Database extends PDO
+{
+}
