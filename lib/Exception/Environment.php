9be1dd055421e1be30fc8c7ae2538a61daa3bb21
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-02-19 15:43:01 +0100

    Add first exceptions.

---
+++ lib/Exception/Environment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sabre\Katana\Exception;
+
+/**
+ * This exception represents any environment issue (like a missing configuration
+ * file).
+ *
+ * @copyright Copyright (C) 2015 fruux GmbH (https://fruux.com/).
+ * @author Ivan Enderlin
+ * @license http://sabre.io/license/ Modified BSD License
+ */
+class Environment extends Exception
+{
+}
---
+++ lib/Exception/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sabre\Katana\Exception;
+
+/**
+ * Mother exception of this project.
+ *
+ * @copyright Copyright (C) 2015 fruux GmbH (https://fruux.com/).
+ * @author Ivan Enderlin
+ * @license http://sabre.io/license/ Modified BSD License
+ */
+class Exception extends \Exception
+{
+}
