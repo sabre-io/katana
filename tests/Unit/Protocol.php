bfaf2cd8fbe2b18df2eb9e5df8a478645122a916
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-02-23 10:23:18 +0100

    Add tests for the `katana://` protocol.

---
+++ tests/Unit/Protocol.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Sabre\Katana\Test\Unit;
+
+use Sabre\Katana\Protocol as LUT;
+
+/**
+ * Test suite of the katana:// protocol.
+ *
+ * @copyright Copyright (C) 2015 fruux GmbH (https://fruux.com/).
+ * @author Ivan Enderlin
+ * @license http://sabre.io/license/ Modified BSD License
+ */
+class Protocol extends Suite
+{
+    public function case_application_public()
+    {
+        $this
+            ->given($path = 'katana://application/public')
+            ->when($result = LUT::realPath($path))
+            ->then
+                ->string($result)
+                    ->isEqualTo(
+                        realpath(
+                            __DIR__ . DS .
+                            '..' . DS .
+                            '..' . DS .
+                            'public'
+                        )
+                    );
+    }
+
+    public function case_application_views()
+    {
+        $this
+            ->given($path = 'katana://application/views')
+            ->when($result = LUT::realPath($path))
+            ->then
+                ->string($result)
+                    ->isEqualTo(
+                        realpath(
+                            __DIR__ . DS .
+                            '..' . DS .
+                            '..' . DS .
+                            'views'
+                        )
+                    );
+    }
+
+    public function case_data_root()
+    {
+        $this
+            ->given($path = 'katana://data/')
+            ->when($result = LUT::realPath($path))
+            ->then
+                ->string($result)
+                    ->isEqualTo(
+                        realpath(
+                            __DIR__ . DS .
+                            '..' . DS .
+                            '..' . DS .
+                            'data'
+                        ) . DS
+                    );
+    }
+}
