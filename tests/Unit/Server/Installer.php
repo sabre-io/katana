658b38603c69885bbeaf920c9345adebf6501298
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-02-24 14:35:54 +0100

    Add tests for `checkBaseUrl` and `checkPasswords`.

--- tests/Unit/Server/Installer.php
+++ tests/Unit/Server/Installer.php
@@ -81,4 +81,81 @@ class Installer extends Suite
                 ->string($response->getBody())
                     ->isNotEmpty();
     }
+
+    public function case_check_correct_base_url()
+    {
+        $this
+            ->given($_baseUrl = $this->realdom->regex('#^/(.+/)?$#'))
+            ->when(function() use($_baseUrl) {
+                foreach ($this->realdom->sampleMany($_baseUrl, 100) as $baseUrl) {
+                    $this
+                        ->boolean($result = LUT::checkBaseUrl($baseUrl))
+                            ->isTrue();
+                }
+            });
+    }
+
+    public function case_check_incorrect_base_url()
+    {
+        $this
+            ->given($_baseUrl = $this->realdom->regex('#[^/].+[^/]$#'))
+            ->when(function() use($_baseUrl) {
+                foreach ($this->realdom->sampleMany($_baseUrl, 100) as $baseUrl) {
+                    $this
+                        ->boolean($result = LUT::checkBaseUrl($baseUrl))
+                            ->isFalse();
+                }
+            });
+    }
+
+    public function case_check_correct_password()
+    {
+        $this
+            ->given($_password = $this->realdom->regex('#[\w\d_!\-@💩]+#'))
+            ->when(function() use($_password) {
+                foreach ($this->realdom->sampleMany($_password, 100) as $password) {
+                    $this
+                        ->given($passwords = $password . $password)
+                        ->boolean($result = LUT::checkPassword($passwords))
+                            ->isTrue();
+                }
+            });
+    }
+
+    public function case_check_incorrect_empty_password()
+    {
+        $this
+            ->given($password = '')
+            ->when($result = LUT::checkPassword($password . $password))
+            ->then
+                ->boolean($result)
+                    ->isFalse()
+
+            ->given($password = null)
+            ->when($result = LUT::checkPassword($password . $password))
+            ->then
+                ->boolean($result)
+                    ->isFalse();
+    }
+
+    public function case_check_incorrect_unmatched_password()
+    {
+        $this
+            ->given(
+                $passwords = [
+                    ['a', 'b'],
+                    ['a', 'aa'],
+                    ['💩', '____']
+                ]
+            )
+            ->when(function() use($passwords){
+                foreach ($passwords as $pair) {
+                    list($password, $confirmed) = $pair;
+                    $this
+                        ->given($result = LUT::checkPassword($password . $confirmed))
+                        ->boolean($result)
+                            ->isFalse();
+                }
+            });
+    }
 }
