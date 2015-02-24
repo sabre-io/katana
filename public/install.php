e74ef77d9863d54080b938136bf93b72d54a29fd
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-02-24 12:02:25 +0100

    Validate the install form on the server-side.

--- public/install.php
+++ public/install.php
@@ -6,6 +6,7 @@ use Sabre\Katana\Server\Installer;
 use Sabre\Katana\Server\Server;
 use Sabre\Katana\Configuration;
 use Sabre\HTTP;
+use Hoa\Router;
 
 /**
  * This file aims at installing the application.
@@ -15,6 +16,7 @@ use Sabre\HTTP;
  * @license http://sabre.io/license/ Modified BSD License
  */
 
+$request  = HTTP\Sapi::getRequest();
 $response = new HTTP\Response();
 
 /**
@@ -31,4 +33,57 @@ if (true === Installer::isInstalled()) {
 
 }
 
-var_dump('go installing!');
+$url   = $request->getUrl();
+$query = '';
+
+/**
+ * We compute asynchronous tasks from the installation page.
+ * They have the following form: install?/<command>
+ *
+ * Else, we print the installation page.
+ */
+if (false !== $pos = strpos($url, '?')) {
+
+    $router = new Hoa\Router\Http();
+    $router
+        ->get(
+            'db',
+            '/baseurl/(?<baseUrl>.*)',
+            function($baseUrl) {
+                echo json_encode(
+                    0 !== preg_match('#/$#', $baseUrl)
+                );
+
+                return;
+            }
+        )
+        ->get(
+            'b',
+            '/password/(?<passwords>.*)',
+            function($passwords) {
+                $length = strlen($passwords);
+
+                if (0 === $length || 0 !== ($length % 2)) {
+
+                    echo json_encode(false);
+                    return;
+
+                }
+
+                echo json_encode(
+                    substr($passwords, 0, $length / 2)
+                    ===
+                    substr($passwords, $length / 2)
+                );
+            }
+        );
+
+    $query = substr($url, $pos + 1);
+    $router->route($query);
+
+    $dispatcher = new Hoa\Dispatcher\Basic();
+    $dispatcher->dispatch($router);
+
+} else {
+    echo file_get_contents('katana://application/views/install.html');
+}
