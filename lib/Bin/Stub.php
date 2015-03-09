c3f620d2594a842995e67f51ead0bb3befb65cc6
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-03-09 09:52:24 +0100

    Move the stub code.

--- lib/Bin/Stub.php
+++ lib/Bin/Stub.php
@@ -7,7 +7,6 @@ use Sabre\Katana\Stub\Phar;
 use Sabre\Katana\Protocol;
 use Sabre\Katana\Exception;
 use Hoa\Console;
-use Hoa\File;
 use Hoa\File\Finder;
 use PharException;
 
@@ -111,9 +110,6 @@ class Stub extends AbstractCommand
             $pathName = 'katana.phar';
             $finder->notIn('/^' . preg_quote($pathName, '/') . '$/');
 
-            $stub = new File\Read(__FILE__);
-            $stub->seek(__COMPILER_HALT_OFFSET__);
-
             $archiveName  =
             $pharPathname = Protocol::realPath('katana://data/share/' . $pathName, false);
 
@@ -123,7 +119,7 @@ class Stub extends AbstractCommand
 
             $phar = new Phar($pharPathname);
             $phar->buildFromIterator($finder, SABRE_KATANA_PREFIX);
-            $phar->setStub(trim($stub->read(1 << 20)) . "\n" . '__HALT_COMPILER();');
+            $phar->setStub($phar->getStubCode());
 
         }
 
@@ -148,141 +144,4 @@ class Stub extends AbstractCommand
                 'help' => 'This help.'
             ]);
     }
-}
-
-__HALT_COMPILER();
-#!/usr/bin/env php
-<?php
-
-Phar::mapPhar('katana.phar');
-
-require 'phar://katana.phar/bootstrap.php';
-
-use Hoa\Router;
-use Hoa\Console;
-use Hoa\Iterator;
-
-$pharPathname = __FILE__;
-$phar         = new Phar(
-    $pharPathname,
-    Phar::KEY_AS_FILENAME |
-    Phar::CURRENT_AS_FILEINFO
-);
-$parser = new Console\Parser();
-$parser->parse(Router\Cli::getURI());
-$options = new Console\GetOption(
-    [
-        ['extract',   Console\GetOption::REQUIRED_ARGUMENT, 'e'],
-        ['overwrite', Console\GetOption::NO_ARGUMENT,       'o'],
-        ['metadata',  Console\GetOption::NO_ARGUMENT,       'm'],
-        ['signature', Console\GetOption::NO_ARGUMENT,       's'],
-        ['list',      Console\GetOption::NO_ARGUMENT,       'l'],
-        ['help',      Console\GetOption::NO_ARGUMENT,       'h'],
-        ['help',      Console\GetOption::NO_ARGUMENT,       '?']
-    ],
-    $parser
-);
-
-function usage()
-{
-    echo
-        'Usage  : ', $_SERVER['argv'][0], ' <options>', "\n",
-        'Options:', "\n",
-        '    -e, --extract=  : Extract the archive into a specific directory.', "\n",
-        '    -o, --overwrite : Overwrite existing files while extracting.', "\n",
-        '    -m, --metadata  : Print metadata of the archive.', "\n",
-        '    -s, --signature : Print the signature of the archive.', "\n",
-        '    -l, --list      : List content of the archive.', "\n",
-        '    -h, --help      : This help.', "\n",
-        '    -?, --help      : This help.', "\n";
-
-    return 0;
-}
-
-$overwrite = false;
-$extractTo = null;
-
-while (false !== $c = $options->getOption($v)) {
-    switch ($c) {
-
-        case '__ambiguous':
-            $options->resolveOptionAmbiguity($v);
-            break;
-
-        case 'e':
-            $extractTo = $v;
-            break;
-
-        case 'o':
-            $overwrite = $v;
-            break;
-
-        case 'm':
-            $metadata = $phar->getMetadata();
-            $max      = 0;
-
-            foreach ($metadata as $key => $value) {
-                $max < $l = strlen($key) and $max = $l;
-            }
-
-            foreach ($metadata as $key => $value) {
-                echo
-                    sprintf(
-                        '%-' . $max . 's ' . "\t" . '%s',
-                        $key,
-                        str_replace("\n", ' ', $value)
-                    ),
-                    "\n";
-            }
-            return 0;
-            break;
-
-        case 's':
-            echo $phar->getSignature()['hash'], "\n";
-            return 0;
-            break;
-
-        case 'l':
-            $prefixLength = strlen('phar://' . $pharPathname);
-            $iterator     = new Iterator\Recursive\Iterator($phar);
-            foreach ($iterator as $value) {
-                echo '/' . $iterator->getSubPathName(), "\n";
-            }
-            return 0;
-            break;
-
-        case 'h':
-        case '?':
-        default:
-            return usage();
-            break;
-
-    }
-}
-
-if (null !== $extractTo) {
-
-    if (false === file_exists($extractTo)) {
-        mkdir($extractTo);
-    }
-
-    if (false === file_exists($extractTo)) {
-
-        echo
-            $extractTo, ' cannot be created.', "\n",
-            'Extraction abort.', "\n";
-        return 1;
-
-    }
-
-    try {
-        return false === $phar->extractTo($extractTo, null, $overwrite);
-    }  catch (PharException $exception) {
-        echo $exception->getMessage();
-        return 1;
-
-    }
-
-} else {
-    return usage();
 }
--- lib/Stub/Phar.php
+++ lib/Stub/Phar.php
@@ -2,6 +2,7 @@
 
 namespace Sabre\Katana\Stub;
 
+use Hoa\File;
 use Phar as PHPPhar;
 
 /**
@@ -41,4 +42,155 @@ class Phar extends PHPPhar
 
         return;
     }
+
+    /**
+     * Get the standard stub.
+     *
+     * @return string
+     */
+    public function getStubCode()
+    {
+        $stub = new File\Read(__FILE__);
+        $stub->seek(__COMPILER_HALT_OFFSET__);
+
+        return trim($stub->read(1 << 20)) . "\n" . '__HALT_COMPILER();';
+    }
+}
+
+__HALT_COMPILER();
+#!/usr/bin/env php
+<?php
+
+Phar::mapPhar('katana.phar');
+
+require 'phar://katana.phar/bootstrap.php';
+
+use Hoa\Router;
+use Hoa\Console;
+use Hoa\Iterator;
+
+$pharPathname = __FILE__;
+$phar         = new Phar(
+    $pharPathname,
+    Phar::KEY_AS_FILENAME |
+    Phar::CURRENT_AS_FILEINFO
+);
+$parser = new Console\Parser();
+$parser->parse(Router\Cli::getURI());
+$options = new Console\GetOption(
+    [
+        ['extract',   Console\GetOption::REQUIRED_ARGUMENT, 'e'],
+        ['overwrite', Console\GetOption::NO_ARGUMENT,       'o'],
+        ['metadata',  Console\GetOption::NO_ARGUMENT,       'm'],
+        ['signature', Console\GetOption::NO_ARGUMENT,       's'],
+        ['list',      Console\GetOption::NO_ARGUMENT,       'l'],
+        ['help',      Console\GetOption::NO_ARGUMENT,       'h'],
+        ['help',      Console\GetOption::NO_ARGUMENT,       '?']
+    ],
+    $parser
+);
+
+function usage()
+{
+    echo
+        'Usage  : ', $_SERVER['argv'][0], ' <options>', "\n",
+        'Options:', "\n",
+        '    -e, --extract=  : Extract the archive into a specific directory.', "\n",
+        '    -o, --overwrite : Overwrite existing files while extracting.', "\n",
+        '    -m, --metadata  : Print metadata of the archive.', "\n",
+        '    -s, --signature : Print the signature of the archive.', "\n",
+        '    -l, --list      : List content of the archive.', "\n",
+        '    -h, --help      : This help.', "\n",
+        '    -?, --help      : This help.', "\n";
+
+    return 0;
+}
+
+$overwrite = false;
+$extractTo = null;
+
+while (false !== $c = $options->getOption($v)) {
+    switch ($c) {
+
+        case '__ambiguous':
+            $options->resolveOptionAmbiguity($v);
+            break;
+
+        case 'e':
+            $extractTo = $v;
+            break;
+
+        case 'o':
+            $overwrite = $v;
+            break;
+
+        case 'm':
+            $metadata = $phar->getMetadata();
+            $max      = 0;
+
+            foreach ($metadata as $key => $value) {
+                $max < $l = strlen($key) and $max = $l;
+            }
+
+            foreach ($metadata as $key => $value) {
+                echo
+                    sprintf(
+                        '%-' . $max . 's ' . "\t" . '%s',
+                        $key,
+                        str_replace("\n", ' ', $value)
+                    ),
+                    "\n";
+            }
+            exit(0);
+            break;
+
+        case 's':
+            echo $phar->getSignature()['hash'], "\n";
+            exit(0);
+            break;
+
+        case 'l':
+            $prefixLength = strlen('phar://' . $pharPathname);
+            $iterator     = new Iterator\Recursive\Iterator($phar);
+            foreach ($iterator as $value) {
+                echo '/' . $iterator->getSubPathName(), "\n";
+            }
+            exit(0);
+            break;
+
+        case 'h':
+        case '?':
+        default:
+            return usage();
+            break;
+
+    }
+}
+
+if (null !== $extractTo) {
+
+    if (false === file_exists($extractTo)) {
+        mkdir($extractTo);
+    }
+
+    if (false === file_exists($extractTo)) {
+
+        echo
+            $extractTo, ' cannot be created.', "\n",
+            'Extraction abort.', "\n";
+        exit(1);
+
+    }
+
+    try {
+        $code = false === $phar->extractTo($extractTo, null, $overwrite);
+    }  catch (PharException $exception) {
+        echo $exception->getMessage();
+        $code = 1;
+    }
+
+    exit($code);
+
+} else {
+    exit(usage());
 }
