32c857b42c9bc66a9ac141fd5129189849c2dcf6
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-03-19 17:50:19 +0100

    Change the license to AGPL-3.0.

--- tests/.bootstrap.atoum.php
+++ tests/.bootstrap.atoum.php
@@ -1,13 +1,33 @@
 <?php
 
+/**
+ * @license
+ *
+ * sabre/katana.
+ * Copyright (C) 2015  fruux GmbH (https://fruux.com/)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 require_once __DIR__ . DIRECTORY_SEPARATOR . '.autoload.atoum.php';
 
 /**
  * This file bootstraps unit tests.
  *
- * @copyright Copyright (C) 2007-2015 fruux GmbH (https://fruux.com/).
+ * @copyright Copyright (C) 2015 fruux GmbH (https://fruux.com/).
  * @author Ivan Enderlin
- * @license http://sabre.io/license/ Modified BSD License
+ * @license GNU Affero General Public License, Version 3.
  */
 
 date_default_timezone_set('UTC');
