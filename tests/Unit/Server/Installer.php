68dd54726a5cdf749bc4b8c0e8004fe962a23cda
Evert Pot <me@evertpot.com>
2015-07-05 00:20:26 -0400

    For some reason php-cs-fixer wants this extra newline.
    Don't care enough to figure out why, so I'm just going to go with it ;)

--- tests/Unit/Server/Installer.php
+++ tests/Unit/Server/Installer.php
@@ -19,6 +19,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace Sabre\Katana\Test\Unit\Server;
 
 use Sabre\Katana\Test\Unit\Suite;
